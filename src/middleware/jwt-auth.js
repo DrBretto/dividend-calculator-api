7e5d838f5e880e8a0b9d7ae6098c8ada9be717f3
Brett Westerlund <drbretto82@gmail.com>
2020-09-08 11:19:21 -0400

    fixing db

--- src/middleware/jwt-auth.js
+++ src/middleware/jwt-auth.js
@@ -1,13 +1,16 @@
 const AuthService = require("../auth/auth-service");
 
 function requireAuth(req, res, next) {
+
   const authToken = req.get("Authorization") || "";
+  console.log("requireAuth -> authToken", authToken);
 
   let bearerToken;
   if (!authToken.toLowerCase().startsWith("bearer ")) {
     return res.status(401).json({ error: "Missing bearer token" });
   } else {
     bearerToken = authToken.slice(7, authToken.length);
+
   }
 
   try {
@@ -15,6 +18,7 @@ function requireAuth(req, res, next) {
 
     AuthService.getUserWithUserName(req.app.get("db"), payload.sub)
       .then((user) => {
+
         if (!user)
           return res.status(401).json({ error: "Unauthorized request" });
 
